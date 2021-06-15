5166c942ed5402832fa636f9d89debcabdfbfd64
Ryan Hules <38119764+rhules@users.noreply.github.com>
2021-06-14 20:54:03 -0700

    fix navbar
    from now on, add name attr to header

--- header.js
+++ header.js
@@ -1,26 +1,42 @@
 $(function () {
+	div = document.getElementById("header");
 	var bar = '';
 	bar += '<nav class="navbar navbar-expand navbar-dark fixed-top bg-primary">';
 	bar += '<a class="navbar-brand" href="#"><img src="Images/Logoshanko_White_Trans_Textless.png" style="width:69px;height:69px"></a>';
 	bar += '<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">';
 	bar += '<span class="navbar-toggler-icon"></span>';
 	bar += '</button>';
-
 	bar += '<div class="collapse navbar-collapse" id="navbarColor01">';
 	bar += '<ul class="navbar-nav mr-auto">';
-	bar += '<li class="nav-item active">';
+	if (div.attributes.getNamedItem("name").value == "index") {
+		bar += '<li class="nav-item active">';
+	}
+	else {
+		bar += '<li class="nav-item">';
+	}
 	bar += '<a class="nav-link" href="https://www.logoshanko.com">Home';
 	bar += '<span class="sr-only">(current)</span>';
 	bar += '</a>';
 	bar += '</li>';
-	bar += '<li class="nav-item">';
+	if (div.attributes.getNamedItem("name").value == "products") {
+		bar += '<li class="nav-item active">';
+	}
+	else {
+		bar += '<li class="nav-item">';
+	}
 	bar += '<a class="nav-link" href="https://www.logoshanko.com/products">Products</a>';
 	bar += '</li>';
-	bar += '<li class="nav-item">';
+	if (div.attributes.getNamedItem("name").value == "about") {
+		bar += '<li class="nav-item active">';
+	}
+	else {
+		bar += '<li class="nav-item">';
+	}
 	bar += '<a class="nav-link" href="https://www.logoshanko.com/about">About Us</a>';
 	bar += '</li>';
 	bar += '</ul>';
 	bar += '</div>';
 	bar += '</nav>';
-	document.getElementById("header").innerHTML += bar;
+
+	div.innerHTML += bar;
 });
